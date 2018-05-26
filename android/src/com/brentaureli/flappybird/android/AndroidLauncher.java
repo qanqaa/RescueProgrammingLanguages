47a883fc84d704d91a6408f1824356a4b3519d56
PawelWojaczek <wojaczek.paw@gmail.com>
2018-05-26 17:17:12 +0200

    Android launching added

--- android/src/com/brentaureli/flappybird/android/AndroidLauncher.java
+++ android/src/com/brentaureli/flappybird/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.brentaureli.flappybird.FlappyBird;
+import com.brentaureli.game.QuizGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new FlappyBird(), config);
+		initialize(new QuizGame(), config);
 	}
 }
--- android/src/com/brentaureli/game/android/AndroidLauncher.java
+++ android/src/com/brentaureli/game/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.brentaureli.game.FlappyDemo;
+import com.brentaureli.game.QuizGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new FlappyDemo(), config);
+        initialize(new QuizGame(), config);
 	}
 }
