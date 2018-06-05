9c0f6a5f7fc8959e31bc348859c69a61c2d0ede7
Kinga <kindzaja@gmail.com>
2018-06-05 12:09:01 +0200

    Fonts in Profile State

--- core/src/com/brentaureli/game/states/ProfileState.java
+++ core/src/com/brentaureli/game/states/ProfileState.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
@@ -20,6 +21,7 @@ public class ProfileState extends State {
     private String nameText = ProfileManager.getInstance().getCurrentProfile().getName();
     private String questionsTimeFieldtext = String.valueOf(ProfileManager.getInstance().getCurrentProfile().getTimeForQuestion());
     private TextField nameField = new TextField(nameText, skin);
+
     private TextField questionsTimeField = new TextField(questionsTimeFieldtext, skin);
     private Texture background;
     private Texture photo;
@@ -67,9 +69,15 @@ public class ProfileState extends State {
             photo = new Texture(Gdx.files.internal(photoPath + "defaultPhoto.png"));
         }
         sb.draw(photo, gameWidth / 2 - PHOTO_WIDTH / 2, gameHeight / 2 - PHOTO_HEIGHT / 4, PHOTO_WIDTH, PHOTO_HEIGHT);
-        font.draw(sb, "NAME", gameWidth / 2 - layout.width, gameHeight - 550);
-        font.draw(sb, "QUESTIONS TIME", gameWidth / 2 - layout.width, gameHeight - 600);
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Autobus-Bold.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 24;
+        BitmapFont font12 = generator.generateFont(parameter);
+        font12.draw(sb, "NAME", gameWidth / 2 - layout.width, gameHeight - 550);
+        font12.draw(sb, "QUESTIONS TIME", gameWidth / 2 - layout.width, gameHeight - 600);
         sb.end();
+
         nameField.setPosition(gameWidth / 2, gameHeight - 580);
         nameField.setSize(150, 40);
         questionsTimeField.setPosition(gameWidth / 2, gameHeight - 630);
@@ -79,6 +87,7 @@ public class ProfileState extends State {
         Gdx.input.setInputProcessor(stage);
         stage.draw();
         stage.act();
+
     }
 
     @Override
