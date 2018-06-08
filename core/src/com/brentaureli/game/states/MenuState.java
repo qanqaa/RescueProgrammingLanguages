55458e87aeacb76c8b72779814a8d31a8dc49025
Kinga <kindzaja@gmail.com>
2018-06-08 15:38:08 +0200

    Android buttons [ highscores ] ERROR

--- core/src/com/brentaureli/game/states/MenuState.java
+++ core/src/com/brentaureli/game/states/MenuState.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Value;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
--- core/src/com/brentaureli/game/states/ScoresState.java
+++ core/src/com/brentaureli/game/states/ScoresState.java
@@ -6,9 +6,17 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.brentaureli.game.QuizGame;
 import com.brentaureli.game.scores.PlayerScore;
 import com.brentaureli.game.scores.PlayerScoreManagerMock;
@@ -25,7 +33,7 @@ public class ScoresState extends State {
     private Texture photo;
     private String photosFolder = "photos/";
 
-    private Texture exitbg;
+    private Texture exitBtn;
 
     private static final int ALL_BUTTONS_WIDTH = 300;
     private static final int ALL_BUTTONS_HEIGHT = 80;
@@ -37,7 +45,11 @@ public class ScoresState extends State {
 
     private Rectangle exit = new Rectangle(ALL_BUTTONS_X, exit_y, ALL_BUTTONS_WIDTH, ALL_BUTTONS_HEIGHT);
 
+    private TextureRegion exitTextureRegion;
+    private TextureRegionDrawable exitTexRegionDrawable;
+    private ImageButton exitButton;
 
+    private Stage stage;
 
     //TODO: AWT working on Android? And why is this awt
 //dla getX() / getY()
@@ -47,7 +59,7 @@ public class ScoresState extends State {
         super(gsm);
         font = new BitmapFont();
         background = new Texture(Gdx.files.internal("highscoresbg.png"));
-        exitbg = new Texture("exitbutton.png");
+        exitBtn = new Texture("exitbutton.png");
         //TODO: IMPLEMENT DATABASE HIGHSCORES
         //playerScoreList = PlayerScoreManager.getTop10HighScores();
         playerScoreList = PlayerScoreManagerMock.getInstance().prepare20Players();
@@ -56,6 +68,32 @@ public class ScoresState extends State {
         }
         defaultPhoto = new Texture(Gdx.files.internal(photosFolder + "defaultPhoto.png"));
 
+
+
+        exitTextureRegion = new TextureRegion(exitBtn);
+        exitTexRegionDrawable = new TextureRegionDrawable(exitTextureRegion);
+        exitButton = new ImageButton(exitTexRegionDrawable); //Set the button up
+
+        stage = new Stage(new ScreenViewport()); //Set up a stage for the ui
+
+        Gdx.input.setInputProcessor(stage); //Start taking input from the ui
+
+        Table table = new Table();
+        table.setFillParent(true);
+        table.bottom();
+
+        table.add(exitButton);
+        table.row();
+
+        stage.addActor(table);
+
+        exitButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y){
+                Vector3 tmp=new Vector3(Gdx.input.getX(),Gdx.input.getY(),0);
+                Gdx.app.log("CLIIIICK", tmp.x + " " + tmp.y);
+                gsm.set(new MenuState(gsm));
+            }
+        });
     }
 
     @Override
@@ -87,17 +125,16 @@ public class ScoresState extends State {
         GlyphLayout layout = new GlyphLayout();
         sb.begin();
         sb.draw(background, 0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        sb.draw(exitbg, QuizGame.WIDTH / 2 - ALL_BUTTONS_WIDTH / 2, EXIT_BUTTON_Y, ALL_BUTTONS_WIDTH, ALL_BUTTONS_HEIGHT);
         int nameWidth = width / 6;
-        int scoreWidth = 2 * width / 3;
+        int scoreWidth = (2 * width / 3)+180;
         int photoWidth = nameWidth - 50;
 
 
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Autobus-Bold.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.size = 40;
+        parameter.size = 120;
         BitmapFont font12 = generator.generateFont(parameter);
-        height -= 200;
+        height -= 800;
         //TODO: photo Manager
         for (PlayerScore playerScore : playerScoreList) {
             if (playerScore.getProfile().getPhoto() != null) {
@@ -105,14 +142,19 @@ public class ScoresState extends State {
             } else {
                 photo = defaultPhoto;
             }
-            height -= 45;
+            height -= 125;
             font12.draw(sb, playerScore.getProfile().getName(), nameWidth+15, height);
             font12.draw(sb, String.valueOf(playerScore.getScore()), scoreWidth+40, height);
-            sb.draw(photo, photoWidth+15, height - 30, 30, 30);
+            sb.draw(photo, photoWidth-100, height - 100, 100, 100);
         }
         sb.end();
+
+        stage.draw();
+        stage.act();
+
         generator.dispose();
         font12.dispose();
+
     }
 
     @Override
