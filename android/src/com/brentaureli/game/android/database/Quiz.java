fa58f3930e398b359e9525720fda91cf581a7c6b
Karolina <Karolina.Mucha@euvic.pl>
2018-06-10 22:18:51 +0200

    question for stage

--- android/src/com/brentaureli/game/android/database/Quiz.java
+++ android/src/com/brentaureli/game/android/database/Quiz.java
@@ -21,8 +21,6 @@ public class Quiz {
     private String answerB;
     @ColumnInfo(name = "correct_Answer")
     private String correctAnswer;
-    @ColumnInfo(name = "category_for_stage")
-    private String category;
 
     public int getId() {
         return id;
@@ -78,13 +76,5 @@ public class Quiz {
 
     public void setCorrectAnswer(String correctAnswer) {
         this.correctAnswer = correctAnswer;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
     }
 }
